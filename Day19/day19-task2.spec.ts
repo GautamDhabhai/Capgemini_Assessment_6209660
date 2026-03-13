28302ca602a75807064f11836fe5de1432127a99
GautamDhabhai <gautamdhabhai@gmail.com>
2026-03-14 00:01:05 +0530

    day 19 tasks

---
+++ Day19/day19-task2.spec.ts
@@ -0,0 +1,14 @@
+import {test, expect} from '../playwright/node_modules/@playwright/test'
+import path from 'path';
+
+test("day19 task 2", async({page})=>{
+    await page.goto('https://demoqa.com/upload-download');
+
+    let [downloadedFile] = await Promise.all([page.waitForEvent('download'), page.locator('#downloadButton').click()]);
+
+    await downloadedFile.saveAs(path.join(__dirname, "../testdata/downloadedFile.txt"));
+    await page.locator('#uploadFile').setInputFiles(path.join(__dirname, "../testdata/downloadedFile.txt"));
+    let filepath = await page.locator('#uploadedFilePath').textContent();
+    await expect(filepath).toContain('downloadedFile.txt');
+    await page.screenshot({path: `screenshots/day19-task2.png`});
+})
---
+++ playwright/tests/day19-task3.spec.ts
@@ -0,0 +1,10 @@
+import {test, expect} from '@playwright/test'
+
+test("day19 task 3", async({page})=>{
+    await page.goto('https://the-internet.herokuapp.com/upload');
+    await page.locator('#file-upload').setInputFiles('C:/Users/Gautam Dhabhai/Desktop/t7a/Capgemini_Assessment_6209660/playwright/testdata/images/itoshi-sae.jpg');
+    await page.locator('#file-submit').click();
+    await expect(page.locator('#uploaded-files')).toContainText('itoshi-sae.jpg');
+    await page.screenshot({path: `screenshots/day19-task3.png`});
+    
+})
